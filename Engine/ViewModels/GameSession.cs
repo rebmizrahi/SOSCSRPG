fa295ca681b8d187016a811b7e4e3c759634e520
Rebecca Mizrahi <becca.mizrahi@gmail.com>
2021-07-30 19:02:57 -0400

    New classes

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            this.CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+
+        }
+
+    }
+}
